dd98a8ec82f8e8a711a45b8e178143044e0a4994
DESKTOP-S1E7OB9\kbin <kbin_xiaobin@163.com>
2021-05-04 17:18:25 +0800

    添加一个泛型useArray

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -46,3 +46,20 @@ export const useDebounce = <V>(value: V, delay?: number) => {
 
     return debouncedValue
 }
+
+
+//编写一个泛型
+export const useArray = <T>(initialArray:T[]) =>{
+    const [value, setValue] = useState(initialArray)
+    return{
+        value,
+        setValue,
+        add: (item:T) => setValue([...value, item]),
+        clear: ()=> setValue([]),
+        removeIndex: (index: number) =>{
+            const copy = [...value]
+            copy.splice(index,1)
+            setValue(copy)
+        }
+    }
+}
